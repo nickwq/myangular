8b845523a47dc05470523285253645bc5b8f2b6e
Wang Qian <nickwq@gmail.com>
2016-01-11 22:46:14 +0800

    add create and get a module

--- test/loader_spec.js
+++ test/loader_spec.js
@@ -56,4 +56,22 @@ describe('modules', function () {
         expect(myModule.requires).toEqual(['myOtherModule']);
 
     });
+
+    it("allows getting a module", function () {
+        var myModule = window.angular.module('myModule', []);
+        var gotModule = window.angular.module('myModule');
+        expect(gotModule).toBe(myModule);
+    });
+
+    it('throws when trying to get a nonexistent module', function() {
+        expect( function()
+        {window.angular.module('nonexistiedModule');}
+        ).toThrow();
+    });
+
+    it("doesnot allow a module to be called hasOwnProperty", function () {
+        expect(function() {
+            window.angular.module('hasOwnProperty', []);
+        }).toThrow();
+    });
 });
