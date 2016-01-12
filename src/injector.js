03d5b6f7cbedb431951243d54cf11e769edda909
Wang Qian <nickwq@gmail.com>
2016-01-12 22:55:22 +0800

    initial for injector and inject constant

---
+++ src/injector.js
@@ -0,0 +1,28 @@
+"use strict";
+
+var _ = require('lodash');
+
+function createInjector(modulesToLoad) {
+    var cache = {};
+    var $provide = {
+        constant: function(key, value){
+            cache[key] = value;
+        }
+    };
+
+    _.forEach(modulesToLoad, function (moduleName) {
+        var module = window.angular.module(moduleName);
+        _.forEach(module._invokeQueue, function(invokeArgs){
+            var method = invokeArgs[0];
+            var args = invokeArgs[1];
+            $provide[method].apply($provide, args);
+        });
+    });
+    return {
+        has: function (key) {
+            return cache.hasOwnProperty(key);
+        }
+    };
+}
+
+module.exports = createInjector;
---
+++ test/injector_spec.js
@@ -0,0 +1,31 @@
+"use strict";
+
+var setupModuleLoader = require('../src/loader');
+
+var createInjector = require('../src/injector');
+
+describe('injector', function () {
+
+    beforeEach(function () {
+        delete window.angular;
+        setupModuleLoader(window);
+    });
+
+    it("can be created", function () {
+        var injector = createInjector([]);
+        expect(injector).toBeDefined();
+    });
+
+    it("has a constant that has been registered to a module", function () {
+        var module = window.angular.module('myModule', []);
+        module.constant('aConstant', 42);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(true);
+    });
+
+    it("does not have a non-registered constant", function () {
+        var module = window.angular.module('myModule', []);
+        var injector = createInjector(['myModule']);
+        expect(injector.has('aConstant')).toBe(false);
+    });
+});
