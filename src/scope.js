00ac934537f636e8db0cc3cbce01af86e38bbfd3
Wang Qian <nickwq@gmail.com>
2016-08-03 00:17:23 +0800

    watch collection should works fine for non-collection

--- src/scope.js
+++ src/scope.js
@@ -282,4 +282,28 @@ Scope.prototype.$destroy = function(){
   }
 };
 
+Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
+    var self = this;
+    var newValue;
+    var oldValue;
+    var changeCount = 0;
+
+    var internalWatchFn = function (scope) {
+        newValue = watchFn(scope);
+
+        if(newValue !== oldValue) {
+            changeCount++;
+        }
+        oldValue = newValue;
+
+        return changeCount;
+    };
+
+    var internalListenerFn = function(){
+        listenerFn(newValue, oldValue, self);
+    };
+
+    return this.$watch(internalWatchFn, internalListenerFn);
+};
+
 module.exports = Scope;
