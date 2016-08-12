9b899d71d1c55750b5728fc55a8728366f6dd213
Wang Qian <nickwq@gmail.com>
2016-08-12 23:02:45 +0800

    watch collection should notice for array like object

--- src/scope.js
+++ src/scope.js
@@ -293,7 +293,7 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
         newValue = watchFn(scope);
 
         if (_.isObject(newValue)) {
-            if (_.isArray(newValue)) {
+            if (_.isArray(newValue) || isArrayLike(newValue)) {
                 if (!_.isArray(oldValue)) {
                     changeCount++;
                     oldValue = [];
@@ -319,6 +319,14 @@ Scope.prototype.$watchCollection = function (watchFn, listenerFn) {
         }
         return changeCount;
     };
+
+    function isArrayLike(obj) {
+        if (_.isNull(obj) || _.isUndefined(obj)) {
+            return false;
+        }
+        var length = obj.length;
+        return _.isNumber(length);
+    }
 
     var internalListenerFn = function () {
         listenerFn(newValue, oldValue, self);
